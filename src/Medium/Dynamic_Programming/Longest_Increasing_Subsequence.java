f41b8f628fef7fa7d623b913991dc5341524708b
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2022-05-12 22:21:47 +0200

    solved

---
+++ src/Medium/Dynamic_Programming/Longest_Increasing_Subsequence.java
@@ -0,0 +1,53 @@
+package Medium.Dynamic_Programming;
+
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+*/
+
+import java.util.Arrays;
+public class Longest_Increasing_Subsequence {
+
+    public static int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j] && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j] + 1;
+                    if(max < dp[i])
+                        max = dp[i];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] x= {10,9,2,5,3,7,101,18};
+        System.out.println(lengthOfLIS(x));
+    }
+}
