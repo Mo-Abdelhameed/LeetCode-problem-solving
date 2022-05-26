63fce8b0d21a7951d01f43c5575ed306bd22ac48
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2022-05-26 17:54:13 +0200

    solved

--- src/Easy/Arrays/Partition_Array_Into_Three_Parts_With_Equal_Sum.java
+++ src/Easy/Arrays/Partition_Array_Into_Three_Parts_With_Equal_Sum.java
@@ -1,4 +1,64 @@
 package Easy.Arrays;
+/*
+Given an array of integers arr, return true if we can partition the array into three non-empty parts with equal sums.
 
+Formally, we can partition the array if we can find indexes i + 1 < j with (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1])
+
+Example 1:
+
+Input: arr = [0,2,1,-6,6,-7,9,1,2,0,1]
+Output: true
+Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+
+Example 2:
+
+Input: arr = [0,2,1,-6,6,7,9,-1,2,0,1]
+Output: false
+
+Example 3:
+
+Input: arr = [3,3,6,5,-2,2,5,1,-9,4]
+Output: true
+Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+
+Constraints:
+
+3 <= arr.length <= 5 * 104
+-104 <= arr[i] <= 104
+*/
 public class Partition_Array_Into_Three_Parts_With_Equal_Sum {
+
+    public static boolean canThreePartsEqualSum(int[] arr) {
+
+        boolean zeros = true;
+        int sum = 0;
+        for(int num : arr) {
+            sum += num;
+            if(zeros && num != 0)
+                zeros = false;
+        }
+        if(zeros)
+            return true;
+        if(sum % 3 != 0)
+            return false;
+
+        int target = sum/3;
+
+        int temp = 0;
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            temp += arr[i];
+            if(temp == target) {
+                count++;
+                temp = 0;
+            }
+        }
+        return count == 3;
+    }
+
+    public static void main(String[] args) {
+        int[] x =
+                {0,0,0,0};
+        System.out.println(canThreePartsEqualSum(x));
+    }
 }
--- src/Medium/Stacks_and_Queues/Minimum_Remove_to_Make_Valid_Parentheses.java
+++ src/Medium/Stacks_and_Queues/Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -1,4 +1,68 @@
 package Medium.Stacks_and_Queues;
 
+
+/*
+Given a string s of '(' , ')' and lowercase English characters.
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either'(' , ')', or lowercase English letter.
+*/
+
+import java.util.HashSet;
+import java.util.Stack;
+
 public class Minimum_Remove_to_Make_Valid_Parentheses {
+
+    public static String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(')
+                stack.push(i);
+            else if (s.charAt(i) == ')' && !stack.isEmpty()) {
+                set.add(i);
+                set.add(stack.pop());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c != '(' && c != ')')
+                sb.append(c);
+
+            else if((c == '(' || c == ')') && set.contains(i))
+                sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minRemoveToMakeValid("a)b(c)d"));
+    }
 }
